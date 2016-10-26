931afb1ee393f07d3d061c66e2ccaa0b299b0677
David Barragán Merino <david.barragan@kaleidos.net>
2016-10-26 17:40:47 +0200

    WIP

--- regenerate.sh
+++ regenerate.sh
@@ -31,7 +31,7 @@ echo "-> Load initial user (admin/123123)"
 python manage.py loaddata initial_user --traceback
 echo "-> Load initial project_templates (scrum/kanban)"
 python manage.py loaddata initial_project_templates --traceback
-echo "-> Generate sample data"
-python manage.py sample_data --traceback
-echo "-> Rebuilding timeline"
-python manage.py rebuild_timeline --purge
+#echo "-> Generate sample data"
+#python manage.py sample_data --traceback
+#echo "-> Rebuilding timeline"
+#python manage.py rebuild_timeline --purge
